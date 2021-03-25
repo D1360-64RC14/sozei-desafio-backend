e92d806149ec18c0be5544037357910f9a930b09
Diego Garcia <gdiego416@gmail.com>
2021-03-25 00:19:33 -0300

    Configurado basicamente o TypeORM

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,12 @@ DotEnv.config();
 // === Padrões ===
 // Porta   : 3000
 // Hostname: localhost
+const DEFAULT_PORT     = "3000";
+const DEFAULT_HOSTNAME = "localhost";
+
 app.listen(
-    parseInt(process.env.SERVER_PORT || "3000"),
-    process.env.SERVER_HOSTNAME || "localhost",
+    parseInt(process.env.SERVER_PORT || DEFAULT_PORT),
+    process.env.SERVER_HOSTNAME || DEFAULT_HOSTNAME,
     () => {
         Logger.success(`Server iniciado na porta ${process.env.SERVER_PORT}`);
     }
